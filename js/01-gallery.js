6b68f7e8faf492b18c43580283c52d8171b72e21
Serhii Bakun <gizzmo599@gmail.com>
2022-09-15 14:16:10 +0300

    Markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const makeGalleryItemMarkup = ({ original, description }) => {
+	return `
+  <div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img class="gallery__image" src="small-image.jpg" data-source="${original}" alt="${description}" />
+		</a>
+	</div>`;
+};
+
+const makeGalleryItems = galleryItems.map(makeGalleryItemMarkup).join("");
+
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("afterbegin", makeGalleryItems);
